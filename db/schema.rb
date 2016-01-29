14c4707796d76e136beadb623035f93530e52a68
karenjho <hokarenj@gmail.com>
2016-01-29 17:59:22 -0500

    Add user names and redesign forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129211010) do
+ActiveRecord::Schema.define(version: 20160129225151) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160129211010) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "name"
   end
 
 end
