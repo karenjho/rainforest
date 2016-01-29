ea88890aa918d72e195ec2749a353abcc324ee48
karenjho <hokarenj@gmail.com>
2016-01-29 16:16:22 -0500

    Move associations in Product model to top for readability

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ActiveRecord::Base
-  validates :name, :description, presence: true
-  validates :price_in_cents, numericality: {only_integer: true}
 
   has_many :reviews
   has_many :users, through: :reviews
+
+  validates :name, :description, presence: true
+  validates :price_in_cents, numericality: {only_integer: true}
 
   def formatted_price
     price_in_dollars = price_in_cents.to_f / 100
