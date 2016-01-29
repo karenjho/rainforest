b554af71a2789d2bd576e36398ae9999d39281b3
karenjho <hokarenj@gmail.com>
2016-01-29 16:15:17 -0500

    Generate Review model and link reviews, users, and products

---
+++ db/migrate/20160129211010_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.text :comment
+      t.belongs_to :product, index: true, foreign_key: true
+      t.belongs_to :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
