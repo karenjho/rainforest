94b884a7b8bfd0bede29569e10abeb1dc2a41f39
karenjho <hokarenj@gmail.com>
2016-01-29 17:51:13 -0500

    Authorize users before they add or delete a review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,16 @@ class ApplicationController < ActionController::Base
 
   private
 
+  helper_method :current_user
+
   def current_user
     current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user
+  def ensure_logged_in
+    unless current_user
+      flash[:alert] = "Please log in."
+      redirect_to login_path
+    end
+  end
 end
